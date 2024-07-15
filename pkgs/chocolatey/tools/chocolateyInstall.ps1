f2e3078340e9fb44ac527190c32b95ebd79d8f82
Hamza Remmal <hamza@remmal.dev>
2024-07-15 18:33:20 +0200

    Update chocolateyInstall.ps1

--- pkgs/chocolatey/tools/chocolateyInstall.ps1
+++ pkgs/chocolatey/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $unzipLocation = Join-Path $unzipLocation "$($env:chocolateyPackageVersion)"  # 
 # Configure the installation arguments
 $packageArgs = @{
   packageName   = 'scala'
-  Url64         = 'https://github.com/scala/scala3/releases/download/3.5.0-RC3/scala3-3.5.0-RC3-x86_64-pc-win32.zip'
+  Url64         = 'https://github.com/scala/scala3/releases/download/3.5.0-RC4/scala3-3.5.0-RC4-x86_64-pc-win32.zip'
   UnzipLocation = $unzipLocation
 }
 
