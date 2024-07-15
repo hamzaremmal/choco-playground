b7ac99f52446e847fd0efe6973d4014e6e5f133b
Hamza Remmal <hamza@remmal.dev>
2024-07-15 17:10:14 +0200

    Update chocolateyInstall.ps1

--- pkgs/chocolatey/tools/chocolateyInstall.ps1
+++ pkgs/chocolatey/tools/chocolateyInstall.ps1
@@ -1,13 +1,11 @@
 $ErrorActionPreference = 'Stop';
+
+$unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
 $packageArgs = @{
   packageName   = 'scala'
-  fileType      = 'MSI'
-  url64bit      = '@LAUNCHER_URL@'
-
-  softwareName  = 'Scala'
-
-  silentArgs    = "/qn /norestart"
-  validExitCodes= @(0)
+  Url64         = 'https://github.com/scala/scala3/releases/download/3.5.0-RC3/scala3-3.5.0-RC3-x86_64-pc-win32.zip'
+  UnzipLocation = $unzipLocation
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgs
