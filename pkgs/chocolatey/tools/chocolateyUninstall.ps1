5fe952068bdb4a495d062e848eb7f7f3dc9b3947
Hamza Remmal <hamza@remmal.dev>
2024-07-15 18:16:06 +0200

    Create chocolateyUninstall.ps1

---
+++ pkgs/chocolatey/tools/chocolateyUninstall.ps1
@@ -0,0 +1,23 @@
+$ErrorActionPreference = 'Stop';
+
+$unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition        # Get the root of chocolatey folder
+$unzipLocation = Join-Path $unzipLocation "$($env:chocolateyPackageName)"     # Append the package's name
+$unzipLocation = Join-Path $unzipLocation "$($env:chocolateyPackageVersion)"  # Append the package's version
+
+
+$extractedDir = Get-ChildItem -Path $unzipLocation | Where-Object { $_.PSIsContainer } | Select-Object -First 1
+
+if (-not $extractedDir) {
+    throw "Failed to find the extracted directory."
+}
+
+# Define the bin path
+$scalaBinPath = Join-Path $unzipLocation $extractedDir | Join-Path -ChildPath 'bin' # Update this path if the structure inside the ZIP changes
+
+# Iterate through the .bat files in the bin directory and create shims
+Write-Host "Creating shims for .bat file"
+Get-ChildItem -Path $(Join-Path $unzipLocation $extractedDir | Join-Path -ChildPath 'bin') -Filter '*.bat' | ForEach-Object {
+    $file = $_.FullName
+    Write-Host "Removing shim for $file..."
+    Uninstall-BinFile -Name $_.BaseName -Path $file
+}
