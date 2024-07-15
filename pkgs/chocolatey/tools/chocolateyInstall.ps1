4582ac14fb8f8e05e6be9a001f5cb8fcb8ebf1ba
Hamza Remmal <hamza@remmal.dev>
2024-07-15 17:17:23 +0200

    Update chocolateyInstall.ps1

--- pkgs/chocolatey/tools/chocolateyInstall.ps1
+++ pkgs/chocolatey/tools/chocolateyInstall.ps1
@@ -9,3 +9,14 @@ $packageArgs = @{
 }
 
 Install-ChocolateyZipPackage @packageArgs
+
+# Define the bin path
+$scalaBinPath = Join-Path $unzipLocation 'scala3-3.5.0-RC3-x86_64-pc-win32' 'bin' # Update this path if the structure inside the ZIP changes
+
+# Iterate through the .bat files in the bin directory and create shims
+Write-Host "Creating shims for .bat files in $scalaBinPath..."
+Get-ChildItem -Path $scalaBinPath -Filter '*.bat' | ForEach-Object {
+    $file = $_.FullName
+    Write-Host "Creating shim for $file..."
+    Install-BinFile -Name $_.BaseName -Path $file
+}
